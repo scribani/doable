e8bbb546f581b2ea86191033964d1c1564fbc76d
Scribani <scribani.andres@gmail.com>
2021-10-09 02:05:34 -0500

    Add constants

---
+++ scripts/constants.js
@@ -0,0 +1,6 @@
+export const BASE_URL = "https://doable-api.herokuapp.com/";
+export const LOGIN = "login";
+export const SIGN_UP = "signUp";
+export const AZ = "alphabetical";
+export const DATE = "due_date";
+export const IMPORTANT = "importance";
